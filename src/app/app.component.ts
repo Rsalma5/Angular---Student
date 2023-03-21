805800a35adf6df820ee2baf322ca29888b4e49d
R <ar-salma.r@rakuten.com>
2023-03-21 16:15:15 +0530

    Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { StudentService } from './student.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  [x: string]: any;
   title = 'StudentDemo';
+
+studentDetails:any;
+
+studentToUpdate = {
+  id:"",
+  name:"",
+  address:"",
+  perc:""
+};
+constructor(private studentService: StudentService){
+  this.getStudentsDetails();
+ }
+register(registerForm: NgForm)
+{
+this.studentService.registerStudent(registerForm.value).subscribe(
+  (resp) =>{
+    console.log(resp);
+    registerForm.reset();
+    this.getStudentsDetails();
+  },
+  (err) => {
+    console.log(err);
+  }
+  
+);
+}
+getStudentsDetails()
+{
+  this.studentService.getStudents().subscribe(
+
+    (resp) =>{
+      console.log(resp);
+      this.studentDetails= resp;
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+}
+ deleteStudent(student: { id: string; })
+ {
+   this.studentService.deleteStudent(student.id).subscribe(
+     (resp)=>{
+       console.log(resp);   
+       this.getStudentsDetails(); 
+     },
+     (err)=>
+     {
+      console.log(err);
+    }
+   );
+ }
+
+ edit(student: any){
+  this['studentToUpdate'] = student;
+ }
+ updateStudent()
+ {
+  this.studentService.updateStudent(this.studentToUpdate).subscribe(
+    (resp) => {
+      console.log(resp);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
+}
 }
